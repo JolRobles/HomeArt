48bb73a77de2cd5f706e0344b64cbfeed0fe6187
JolRobles <jluis4013@gmail.com>
2021-06-26 18:11:04 -0500

    cascaron del proyeccto

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns =[
     path('',views.index, name ='index'),
+    path('socio',views.socio, name ='socio'),
+    path('about',views.about, name ='about'),
+    path('contact',views.contact, name ='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'index.html')
+
+def socio(request):
+
+    return render(request, 'socio.html')
+
+def about(request):
+
+    return render(request, 'about.html')
+
+def contact(request):
+
+    return render(request, 'contact.html')
